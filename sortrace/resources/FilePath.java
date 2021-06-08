432ac0959773fc57173c232b6105c6c62b5d26ea
EduExposit <48351097+EduExposit@users.noreply.github.com>
2021-06-08 20:49:10 +0200

    Add files via upload

---
+++ sortrace/resources/FilePath.java
@@ -0,0 +1,40 @@
+package main.resources;
+
+public class FilePath {
+    public static final String PROPERTIES_PATH = "/main/resources/Configuracion/properties/";
+    public static final String SPANISH_PROPERTIES = "/main/resources/Configuracion/properties/castellano.properties";
+    public static final String ENGLISH_PROPERTIES = "/main/resources/Configuracion/properties/ingles.properties";
+    public static final String CONFIGURACION_PROPERTIES = "/main/resources/properties/configuracionIU.properties";
+    public static final String ICONS_PATH = "/main/resources/icons/";
+    public static final String PAGINAS_AYUDA = "/main/resources/ayuda/paginas/";
+    public static final String ICONO_ABANDONAR_EJECUCION = "AbandonarEjecucion.png";
+    public static final String ICONO_AJUSTES_ANIMACION = "AjustesAnimacion.png";
+    public static final String ICONO_ANIMACION = "Animacion.png";
+    public static final String ICONO_AVANZAR_CONTINUO = "AvanzarContinuo.png";
+    public static final String ICONO_AVANZAR_FINAL = "AvanzarFinal.png";
+    public static final String ICONO_AVANZAR_ITERACION = "AvanzarIteracion.gif";
+    public static final String ICONO_AVANZAR_PASO = "AvanzarPaso.gif";
+    public static final String ICONO_AYUDA = "Ayuda.png";
+    public static final String ICONO_DATOS_ALEATORIOS = "DatosAleatorios.png";
+    public static final String ICONO_DATOS_FICHERO = "DatosFichero.png";
+    public static final String ICONO_GUARDAR = "Guardar.png";
+    public static final String ICONO_IDIOMA = "Idioma.png";
+    public static final String ICONO_IMAGEN = "Imagen.png";
+    public static final String ICONO_INFORMACION = "Informacion.png";
+    public static final String ICONO_INTERROGACION = "Interrogacion.gif";
+    public static final String ICONO_INICIO_ES = "LogoInicioEs.png";
+    public static final String ICONO_INICIO_EN = "LogoInicioEn.png";
+    public static final String ICONO_MODIFICAR_DATOS = "ModificarDatos.png";
+    public static final String ICONO_PAUSA = "Pausa.gif";
+    public static final String ICONO_RETROCEDER_COMIENZO = "RetrocederComienzo.png";
+    public static final String ICONO_RETROCEDER_CONTINUO = "RetrocederContinuo.png";
+    public static final String ICONO_RETROCEDER_ITERACION = "RetrocederIteracion.gif";
+    public static final String ICONO_RETROCEDER_PASO = "RetrocederPaso.png";
+    public static final String ICONO_SALIR = "Salir.png";
+    public static final String ICONO_SECUENCIA_IMAGENES = "SecuenciaImagenes.jpg";
+    public static final String ICONO_TECLADO = "Teclado.png";
+    public static final String ICONO_VISUALIZACION = "Visualizacion.png";
+
+    public FilePath() {
+    }
+}
