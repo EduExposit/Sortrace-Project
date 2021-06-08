17b711843f94879499659d3a50b93f771937380c
EduExposit <48351097+EduExposit@users.noreply.github.com>
2021-06-08 20:50:26 +0200

    Add files via upload

---
+++ sortrace/resources/ayuda/IU_Ayuda.java
@@ -0,0 +1,20 @@
+package main.resources.ayuda;
+
+import java.awt.Component;
+import javax.swing.JFrame;
+
+public class IU_Ayuda extends JFrame {
+    private PanelAyuda panel;
+
+    public IU_Ayuda() {
+        this.setTitle("Ayuda de Sortrace");
+        this.setSize(700, 450);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(2);
+        this.panel = new PanelAyuda();
+        this.setContentPane(this.panel);
+        this.setLocationRelativeTo((Component)null);
+        this.pack();
+        this.setVisible(true);
+    }
+}
